f7b112614c2c851847ef74cc3da320ea4b8f81a4
João batista <joao.junior@certsys.com.br>
2020-07-22 14:08:52 -0300

    fix: get tasks

--- src/app/controllers/taskController.js
+++ src/app/controllers/taskController.js
@@ -1,29 +1,37 @@
-const Task = require('../models/task');
+const Task = require("../models/task");
 
 module.exports = {
   async index(request, response) {
     try {
-      const task = await Task.find().populate(['user', 'tasks']);
-  
-      return response.send({ task });
+      const task = await Task.find().populate(["user", "tasks"]);
+      content = [];
+      task.forEach((item) => {
+        content.push({
+          title: item.title,
+          description: item.description,
+          date: item.dueDate,
+          value: item.value,
+        });
+      });
+      // console.log(content);
+      return response.send({ content });
     } catch (err) {
-      return response.status(400).send({ error: 'Error loading tasks' });
+      return response.status(400).send({ error: "Error loading tasks" });
     }
   },
 
   async create(request, response) {
-    console.log('request ===>', request.body);
+    console.log("request ===>", request.body);
     const { title, description, value, dueDate } = request.body.params;
 
     try {
       const task = await Task.create(request.body.params);
 
       return response.send({
-        task
+        task,
       });
     } catch (err) {
-      return response.status(400).send({ error: 'Registration task failed' });
+      return response.status(400).send({ error: "Registration task failed" });
     }
   },
-
-}
+};
